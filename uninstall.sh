637cc04c55244f50f8d4d9234b3c6ec7fa62d5b3
khvalera <khvalera@ukr.net>
2026-01-30 01:43:03 +0200

    uninstall.sh ARCH

--- uninstall.sh
+++ uninstall.sh
@@ -11,7 +11,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 # Configuration
-INSTALL_PREFIX="/usr/local"
+INSTALL_PREFIX="/usr"
 CONFIG_DIR="/etc/port-forward"
 LOCALE_DIR="/usr/share/port-forward/locale"
 
